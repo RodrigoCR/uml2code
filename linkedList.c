8220427f93f8fda98dc24b6baff50bc97afcd615
rodrigo.ltd <rodrigo.ltd@1535685e-18ba-11de-896d-b39e94f1936c>
2009-04-06 20:01:13 +0000

    New linked list structure

---
+++ linkedList.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Linked list node
+struct node {
+	char *line;
+	struct node *next;
+};
+
+// Linked List structure
+struct LinkedList {
+	struct node *first;
+	int length;
+};
+
+// Inserts a string at the start of the list
+struct LinkedList *insert(struct LinkedList *list, char *text) {
+	if(list == NULL) {
+		printf("Initializing list on method insert\n");
+		list = (struct LinkedList *)malloc(sizeof(struct LinkedList));
+		list -> first = NULL;
+		list -> length = 0;
+	}
+	struct node *temp;
+	if(list -> first == NULL){
+		(list -> first) = (struct node *)malloc(sizeof(struct node));
+		// Just in case of error
+		if(list -> first == NULL) {
+			printf("== Error assingning memory ==\n");
+			exit(0);
+		}
+		(list -> first) -> line = text;
+		(list -> first) -> next = NULL;
+		(list -> length)++;
+	} else {
+		temp = list -> first;
+		while (temp -> next != NULL) {
+			temp = temp -> next;
+		}
+		temp -> next = (struct node *)malloc(sizeof(struct node));
+		// Just in case of error
+		if(temp -> next == NULL){
+			printf("== Error assingning memory ==\n");
+			exit(0);
+		}
+		temp = temp -> next;
+		temp -> line = text;
+		temp -> next = NULL;
+		(list -> length)++;
+	}
+	return list;
+}
+
+void printlist(struct LinkedList *list){
+	struct node *temp;
+	if(list == NULL) {
+		printf("Initializing list on method print\n");
+		list = (struct LinkedList *)malloc(sizeof(struct LinkedList));
+		list -> first = NULL;
+		list -> length = 0;
+	}
+	temp = list -> first;
+	int j = 0;
+	if(list -> first != NULL){
+		printf("The text lines in the list are:\n");
+		do{
+			printf("[%d] - %s", j, temp -> line);
+			if(temp -> next != NULL)
+				printf("\n");
+			temp = temp -> next;
+			j++;
+		} while (temp != NULL);
+		printf("\n");
+	} else {
+		printf("The list is empty (%d elements)\n", list -> length);
+	}
+}
+
+char *elementAt(int index, struct LinkedList *list){
+	if(list == NULL) {
+		printf("Initializing list on method elementAt\n");
+		list = (struct LinkedList *)malloc(sizeof(struct LinkedList));
+		list -> first = NULL;
+		list -> length = 0;
+	}
+	struct node *temp;
+	int i = 0;
+	if(index < (list -> length) && index >= 0) {
+		printf("Searching index %d...\n", index);
+		temp = list -> first;
+		while(i < index){
+			temp = temp -> next;
+			i++;
+		}
+		return temp -> line;
+	} else {
+		printf("That node doesn't exist\n");
+		return NULL;
+	}
+}
+
+// Erase jumplines on a string
+char *ejlines(char original[]){
+	int j, k = 0, saltos = 0, nleng;
+	char *new = NULL;
+	for(j = 0; j < strlen(original); j++){
+		if(original[j] == '\\'){
+			if(original[j+1] == 'n'){
+				saltos++;
+				j++;
+			}
+		}
+	}
+	nleng = strlen(original) - (saltos*2) + 1;
+	new = malloc(sizeof(char)*nleng);
+	for(j = 0; j < strlen(original); j++){
+		if(original[j] == '\\'){
+			if(original[j+1] == 'n'){
+				j++;
+			}
+		} else {
+			new[k] = original[j];
+			k++;
+		}
+	}
+	return new;
+}
+
+int main() {
+	printf("*** String-linked-list by RoD ***\n\n");
+	printf("Demo of a string's list:\n");
+	struct LinkedList *demo = NULL;
+	demo = insert(demo, "Linea uno");
+	demo = insert(demo, "Linea dos");
+	demo = insert(demo, "Linea tres");
+	printf("La segunda linea dice: '%s'\n", elementAt(1,demo));
+	printlist(demo);
+	
+	
+	// File name length of 500
+	char name_of_file[500];
+	printf("*\nNow you can write the name a the file to open:\n");
+	scanf("%s", name_of_file);
+	FILE *f;
+	f = fopen(name_of_file, "r");
+	if(f == NULL) {
+		printf("Can't open the file ('%s')\nExiting now...\n", name_of_file);
+		return 0;
+	}
+	printf("Opening file '%s' [ OK ]\n", name_of_file);
+	char buffer[151];
+	struct LinkedList *start = NULL;
+	while(!feof(f)){
+		if(fgets(buffer,150,f)){
+			char *nbuf = NULL;
+			nbuf = ejlines(buffer);
+			printf("Inserting: %s\n", buffer);
+			start = insert(start, buffer);
+		}
+	}
+	printf("End of file\n");
+	fclose(f);
+
+	//printlist(start);
+	
+	
+	return 0;
+}
